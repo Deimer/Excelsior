fc7b76b793a57ee4022e50c75c3be51ae5f286d8
Deimer Villa Pedraza <deymer.villa@petroprix.com>
2022-08-23 00:28:20 -0500

    The new navigation resource was added to initialize the main flows of the app and the main activity was adapted to implement the FragmentContainerView and with it the navigation resource.

--- app/src/main/java/test/deymer/excelsior/ui/MainActivity.kt
+++ app/src/main/java/test/deymer/excelsior/ui/MainActivity.kt
@@ -2,12 +2,17 @@ package test.deymer.excelsior.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import test.deymer.excelsior.R
+import dagger.hilt.android.AndroidEntryPoint
+import test.deymer.excelsior.databinding.ActivityMainBinding
 
+@AndroidEntryPoint
 class MainActivity: AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
